0a5b9ae157c0fb09f4f8e2cec92aabe23f34a1be
Shashi Khanal <khanalshashi@gmail.com>
2024-10-18 16:53:56 -0400

    integration and unit test separated, code coverage added

--- jest.integration.config.ts
+++ jest.integration.config.ts
@@ -5,6 +5,17 @@ const config: Config = {
   testEnvironment: 'node',
   roots: ['./tests/integration'],
   verbose: true,
+  collectCoverage: true,
+  coverageDirectory: `./tests/reports/int-coverage-report-${Date.now()}`,
+  coverageReporters: ['text', 'html'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    }
+  }
 }
 
 export default config;
--- jest.unit.config.ts
+++ jest.unit.config.ts
@@ -5,6 +5,17 @@ const config: Config = {
   testEnvironment: 'node',
   roots: ['./tests/unit'],
   verbose: true,
+  collectCoverage: true,
+  coverageDirectory: `./tests/reports/unit-coverage-report--${Date.now()}`,
+  coverageReporters: ['text', 'html'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    }
+  }
 }
 
 export default config;
