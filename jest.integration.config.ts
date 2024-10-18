9514904d30f14d107ad706bb884d8d1e34b11f90
Shashi Khanal <khanalshashi@gmail.com>
2024-10-18 03:57:11 +0000

    unit and integration test run separated

--- jest.integration.config.ts
+++ jest.integration.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['./tests'],
+  roots: ['./tests/integration'],
   verbose: true,
 }
 
